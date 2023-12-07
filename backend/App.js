ea58b937b04911b4c182043db712ec406588ba78
Zahwa Ait <zahwaaitennecer@gmail.com>
2023-12-07 22:15:20 +0100

    added contactModel and contactRoutes in the backend

--- backend/App.js
+++ backend/App.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 require("./cnx/cnx");
-const auth = require("./routes/auth")
-const list = require("./routes/list")
+
+const contactRoutes = require("./routes/contactRoutes");
+const auth = require("./routes/auth");
+const list = require("./routes/list");
 app.use(express.json());
-app.get("/",(req,res)=>{
-    res.send("hello")
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
-app.use("/api/v1",auth);
-app.use("/api/v2",list);
-app.listen(1000,() => {
+app.use("/api/v1", auth);
+app.use("/api/v2", list);
+// Use the contact route
+app.use("/api/contact", contactRoutes);
+app.listen(3000, () => {
   console.log("started");
-}
-);
+});
